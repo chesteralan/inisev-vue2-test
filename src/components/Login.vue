10013c46ff1089e1d480cb6d19ef46ba56db9f90
Chester Alan <chesteralantagudin@gmail.com>
2022-08-02 15:30:01 +0800

    initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login">
+    <h3>Sign in to your account</h3>
+    <div v-if="error" class="error">Login error</div>
+    <div><input type="text" v-model="username" /></div>
+    <div><input type="password" v-model="password" /></div>
+    <button v-on:click="triggerLogin">Sign In</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      password: '',
+      error: false
+    }
+  },
+  methods: {
+    triggerLogin: function () {
+      if (this.username === 'admin' && this.password === '1234') {
+        return this.$router.push({ name: 'Users' })
+      }
+      this.error = true
+      setTimeout(function () {
+        this.error = false
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error {
+  color: red;
+  font-size: 10px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import Users from '@/components/Users'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/users',
+      name: 'Users',
+      component: Users
+    }
+  ]
+})
